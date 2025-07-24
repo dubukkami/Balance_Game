a589a52f0e672f26d99b47856b346a315176f527
khm <zqxwcerv@gmail.com>
2025-07-25 04:13:27 +0900

    패키지 명 수정

--- backend/src/main/java/com/drink/balancegame/BalanceGameApplication.java
+++ backend/src/main/java/com/drink/balancegame/BalanceGameApplication.java
@@ -2,8 +2,10 @@ package com.drink.balancegame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.drink.balancegame.repository")
 public class BalanceGameApplication {
     public static void main(String[] args) {
         SpringApplication.run(BalanceGameApplication.class, args);
